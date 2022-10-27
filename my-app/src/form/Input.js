07dbd60564e61478a8833dba20bc5e6c001f9bcb
Arieleketely <arieleketely2012@hotmail.com>
2022-10-27 16:22:20 -0300

    Projeto React

---
+++ my-app/src/form/Input.js
@@ -0,0 +1,11 @@
+import styles from './Input.module.css'
+
+function Input({type, text,name,placeholder,handleOnChange, value}){
+    return(
+        <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+    )
+}
+export default Input
